fba62986beeb62e7f8a5f2dee13712aa8123a394
Yar Night <yaroslav239@gmail.com>
2023-12-13 18:58:46 +0300

    feat: add queue task

--- src/queue.js
+++ src/queue.js
@@ -13,21 +13,47 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
+class ListNode {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 class Queue {
 
+  constructor(){
+    this.start = null;
+    this.finish = null;
+  }
+  
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.finish;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue( value ) {
+    let node = new ListNode(value);
+
+    if (!this.finish) {
+      this.finish = node;
+      this.start = node;
+    } else {
+      this.start.next = node;
+      this.start = node;
+    }
+    return this.getUnderlyingList();
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.finish) {
+      return null;
+    }
+    let result = this.finish.value;
+    this.finish = this.finish.next;
+
+    if (!this.finish) {
+      this.start = null;
+    }
+    return result;
   }
 }
 
